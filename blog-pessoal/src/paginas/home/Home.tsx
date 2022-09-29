9c8d4f5be563875c4fd19433ea5b969a265a8b8c
unknown <leticiadearaujo005@gmail.com>
2022-09-29 14:31:26 -0300

    Criando Home

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ffotos-vetores-gratis%2Fwallpaper-desktop&psig=AOvVaw1SmSMyj1gifsvx7vYoLVKH&ust=1664558799356000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPCugZ3DuvoCFQAAAAAdAAAAABAJ" alt ="Imagem inicial" className="img"/>   
+        </>
+    );
+}
+
+export default Home;
