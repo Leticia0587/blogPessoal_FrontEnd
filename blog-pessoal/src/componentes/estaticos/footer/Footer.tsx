4cb3c9cc93a3130f74e55938509105c3ac06c6c9
unknown <leticiadearaujo005@gmail.com>
2022-10-06 14:20:47 -0300

    Navbar

---
+++ blog-pessoal/src/componentes/estaticos/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import purple from "@material-ui/core/colors/purple";
+import "./Footer.css";
+
+
+function footer() {
+  return (
+    <>
+  
+    </>
+  );
+}
+
+export default footer;
--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import './Home.css'
+import {Grid, Typography, Button, Box } from '@material-ui/core';
+import './Home.css';
 
 function Home() {
     return (
         <>
-        <h1 className="titulo">Home</h1>
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ffotos-vetores-gratis%2Fwallpaper-desktop&psig=AOvVaw1SmSMyj1gifsvx7vYoLVKH&ust=1664558799356000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPCugZ3DuvoCFQAAAAAdAAAAABAJ" alt ="Imagem inicial" className="img"/>   
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5"}}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20}>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold"}}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold"}}>Expressse aqui seus pensamentos e opniôes!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white"}}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px"/>
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white"}}>
+                </Grid>
+            </Grid>
         </>
     );
 }
