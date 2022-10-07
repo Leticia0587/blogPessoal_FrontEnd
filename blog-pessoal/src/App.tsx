33b72e21412a42c03da439ad9c8e1175f89c4f8e
unknown <leticiadearaujo005@gmail.com>
2022-10-07 16:07:43 -0300

    Criando Service e UserLongin

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
-// import CadastroUsuario from './paginas/cadastro/CadastroUsuario';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from "./paginas/login/Login";
 
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+            <Route path="/cadastro" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
